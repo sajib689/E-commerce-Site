1d4db5943042ce0b5fbfb148358d039d55e9b46e
sajib689 <jibonraja11@gmail.com>
2023-01-25 01:55:01 +0600

    last commit

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-// const header = document.querySelector('.header');
+// const menuIcon = document.getElementById('menu-icon');
+// const menu = document.getElementById('menu');
 
-// window.addEventListener('scroll', function() {
-//     header.classList.toggle('sticky', this.window.scrollY > 0);
+// menuIcon.addEventListener('click',()=> {
+//     if (menu.className === 'open'){
+//         menu.classList.remove('open');
+//         menu.classList.toggle('bx-x')
+//     } else{
+//         menu.classList.add('open');
+//     }
 // })
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+};
